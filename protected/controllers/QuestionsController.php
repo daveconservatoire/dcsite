e7b2890e308775cee33da3863f2dbdbc65048536
David Rees <d.w.rees.03@gmail.com>
2013-06-05 11:33:21 +0100

    Resolved questions 404s replaced with 410s

---
+++ protected/controllers/QuestionsController.php
@@ -0,0 +1,28 @@
+<?php
+
+class QuestionsController extends Controller
+{
+	/**
+	 * Declares class-based actions.
+	 */
+
+
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		throw new CHttpException(410,'Unfortunately due to frequent spam, I\'ve had to close the questions section for the time being.  It\'ll be back soon!');
+	}
+	
+
+	}
+	
+	?>
+	
+	
+	
