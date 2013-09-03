d13b89d4f9ae91429d462961a7811218bbb8d19a
David Rees <d.w.rees.03@gmail.com>
2013-09-03 17:48:00 +0100

    maybe this time?

--- js/khan-exercise.js
+++ js/khan-exercise.js
@@ -989,7 +989,7 @@ lowLag.load(urlBase+"sounds/"+exerciseUrlTitle+"/"+i+".wav", 'sound'+i);
         })();
     } else {
         soundManager.setup({
-  url: urlBase+'/swf/',
+  url: urlBase+'swf/',
   flashVersion: 9, // optional: shiny features (default = 8)
   // optional: ignore Flash where possible, use 100% HTML5 mode
   // preferFlash: false,
