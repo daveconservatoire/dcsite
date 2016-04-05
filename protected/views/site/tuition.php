9f0a1a52c7e62562710139a55b574b937a005a59
David Rees <d.w.rees.03@gmail.com>
2016-04-05 12:39:10 +0100

    text changes

--- protected/views/site/tuition.php
+++ protected/views/site/tuition.php
@@ -37,7 +37,7 @@ $this->pageTitle='Contact | '.Yii::app()->name ;
 <div class="row">
 	
 <div class="span6">
-<p>	Students on Dave Conservatoire often ask for some extra help on their musical journey.  It is very reassuring to know you're on the right path, have that bit of reassurance before the big test or get some honest feedback about your performance or composition work.  </p>
+<p>	Students on Dave Conservatoire often ask for some extra help on their musical journey.  It is very reassuring to know you're on the right path, have that bit of guidance before the big test or get some regular honest feedback about your performance or composition work.  </p>
 
 <p>For that reason, I am pleased to offer personal music tutoring via Skype or Google Hangout.  This works surprisingly well and allows anyone to access a music teacher at a time that suits them and at low cost.  Price start from $40 for a single 30 minute session and discounts for multiple bookings are available.</p>  
 
