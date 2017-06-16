27fa94767fbcd10816f27bbe23a7ab4241f5c77d
David Rees <d.w.rees.03@gmail.com>
2017-06-16 11:22:40 +0100

    add feedback form

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 <title>Dave Conservatoire</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="icon" href="<?=bu();?>/img/dcfavicon.ico" type="image/x-icon" /> 
+<link rel="icon" href="<?=bu();?>/img/dcfavicon.ico" type="image/x-icon" />
 <meta name="description" content="<?php echo CHtml::encode($this->extraDesc); ?>Dave Conservatoire offers free lessons and interactive exercises on music theory." >
 <meta name="keywords" content="<?php echo CHtml::encode($this->extraKeywords); ?>free music lessons, free music theory, open learning, music history, music notation lessons" />
 <meta property="fb:page_id" content="199558400134104" />
@@ -40,7 +40,7 @@
 
 <script src="<?=bu();?>/js/modernizr.custom.js"></script>
 
-<script src="<?=bu();?>/js/jquery.js"></script>	
+<script src="<?=bu();?>/js/jquery.js"></script>
 <script src="<?=bu();?>/js/bootstrap.min.js"></script>
 <script src="<?=bu();?>/js/cookie.js"></script>
 <script src="<?=bu();?>/js/bootstrap-slider.js"></script>
@@ -69,7 +69,7 @@ if($user->subamount>0):
 $showBanner=False;
 endif;
 endif;
-						
+
 
 
 
@@ -91,17 +91,17 @@ if (rand(0, 1)==0):
          <div class="span4">
 	       <? if (false):?>
             <p id="patreonpitch">If you have found this site useful, <b>make it available for future music students by donating:</b>
-               <a class="btn btn-large dc-btn-yellow" id="10Button">$10</a> 
-               <a class="btn btn-large dc-btn-orange" id="20Button">$20</a> 
-               <a class="btn btn-large dc-btn-redorange" id="50Button">$50</a> 
+               <a class="btn btn-large dc-btn-yellow" id="10Button">$10</a>
+               <a class="btn btn-large dc-btn-orange" id="20Button">$20</a>
+               <a class="btn btn-large dc-btn-redorange" id="50Button">$50</a>
             <form action="<? echo Yii::app()->request->baseUrl;?>/charge" method="POST" id="payment-form">
                <input type="hidden" name="amount" value="0" id="chargeamount">
             </form>
             <? endif;?>
             <!--
              <p>If you've found this site useful, <b>make it available for future students <br />(and remove these annoying banners) by donating via Patreon:</b>
-	                <a class="btn btn-large dc-btn-orange" href="http://patreon.com/daveconservatoire" target="_blank" id="patreonbutton">Give to Dave Conservatoire via Patreon.com</a> 
-	                
+	                <a class="btn btn-large dc-btn-orange" href="http://patreon.com/daveconservatoire" target="_blank" id="patreonbutton">Give to Dave Conservatoire via Patreon.com</a>
+
 
 
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypalform">
@@ -143,11 +143,11 @@ if (rand(0, 1)==0):
 </form>
 -->
 <a href="<? echo Yii::app()->request->baseUrl; ?>/subscribe" class="btn btn-primary" style="margin-top: 43px"><h2>Click to Subscribe!</h2>
-            
+
 
 </a>
 	                </div>
-	             
+
          </div>
       </div>
    </div>
@@ -159,24 +159,24 @@ if (rand(0, 1)==0):
    <div class="container">
       <div class="row">
          <div class="span8">
-            <h3>Need some extra help, guidance or feedback?</h3>
-            <h3>We now offer personal tuition at all levels of ability</h3>
-            <p>Sometimes it is good to know you're on the track, book in a lesson with Dave Conservatoire founder David Rees!</p>
+            <h3>Dave Conservatoire needs YOU!</h3>
+            <h3>Help us to improve this site</h3>
+            <p>Got 5 minutes?  Please complete our feedback survey!</p>
          </div>
          <div class="span4">
 	       <? if (false):?>
             <p id="patreonpitch">If you have found this site useful, <b>make it available for future music students by donating:</b>
-               <a class="btn btn-large dc-btn-yellow" id="10Button">$10</a> 
-               <a class="btn btn-large dc-btn-orange" id="20Button">$20</a> 
-               <a class="btn btn-large dc-btn-redorange" id="50Button">$50</a> 
+               <a class="btn btn-large dc-btn-yellow" id="10Button">$10</a>
+               <a class="btn btn-large dc-btn-orange" id="20Button">$20</a>
+               <a class="btn btn-large dc-btn-redorange" id="50Button">$50</a>
             <form action="<? echo Yii::app()->request->baseUrl;?>/charge" method="POST" id="payment-form">
                <input type="hidden" name="amount" value="0" id="chargeamount">
             </form>
             <? endif;?>
             <!--
              <p>If you've found this site useful, <b>make it available for future students <br />(and remove these annoying banners) by donating via Patreon:</b>
-	                <a class="btn btn-large dc-btn-orange" href="http://patreon.com/daveconservatoire" target="_blank" id="patreonbutton">Give to Dave Conservatoire via Patreon.com</a> 
-	                
+	                <a class="btn btn-large dc-btn-orange" href="http://patreon.com/daveconservatoire" target="_blank" id="patreonbutton">Give to Dave Conservatoire via Patreon.com</a>
+
 
 
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypalform">
@@ -217,12 +217,12 @@ if (rand(0, 1)==0):
 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
 -->
-<a href="<? echo Yii::app()->request->baseUrl; ?>/tuition" class="btn btn-primary" style="margin-top: 43px"><h2>Find out more!</h2>
-            
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfvbt1mP1r-QqzGHTj-wGa7zKx6O7WEVsVtVaas6YZ_JJgutQ/viewform" class="btn btn-primary" style="margin-top: 43px"><h2>Take the survey!</h2>
+
 
 </a>
 	                </div>
-	             
+
          </div>
       </div>
    </div>
@@ -231,24 +231,24 @@ if (rand(0, 1)==0):
 
 <? endif;?>
 <? endif;?>
-      
-      
+
+
    </div>
 <!--header-->
 <div class="navbar">
   <div class="navbar-inner visible-phone">
     <div class="container">
- 
+
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <a class="btn btn-navbar" data-toggle="collapse" data-target="#mainfoldout">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-     
+
       <!-- Be sure to leave the brand out there if you want it shown -->
       <a class="brand mobilenav" href="http://www.daveconservatoire.org">  <span class="mobiledc">Dave Conservatoire</span></a>
- 
+
       <!-- Everything you want hidden at 940px or less, place within here -->
   <div class="nav-collapse collapse navbar-responsive-collapse" id="mainfoldout">
                     <ul class="nav">
@@ -257,26 +257,26 @@ if (rand(0, 1)==0):
 						<a href="/tuition" class="btn btn-block dc-btn-redorange">Personal Tuition</a>
 						<a href="/contact" class="btn btn-block dc-btn-red">Contact</a>
 						<?php if (Yii::app()->user->isGuest &&  !isset($_COOKIE['dc_tempusername'])): ?>
-						
+
 						<a href="<? echo Yii::app()->request->baseUrl;?>/login" class="btn btn-block dc-btn-red loginbutton">Login</a>
-						
-						<?php 
+
+						<?php
 						endif;
 						if (Yii::app()->user->isGuest &&  isset($_COOKIE['dc_tempusername'])
-						): 
+						):
 						 $user=User::model()->findByAttributes(array('username'=>$_COOKIE['dc_tempusername']));
 						 if ($user->points>1):
 						 ?>
 						 <a class="btn btn-success btn-block loginbutton" href="<? echo Yii::app()->request->baseUrl; ?>/login"><i class="icon-exclamation-sign icon-white"></i> Unclaimed points. Login to save your progress</a>
 						 <? else: ?>
 						 <a href="<? echo Yii::app()->request->baseUrl;?>/login" class="btn dc-btn-red btn-block loginbutton">Login</a>
-						 <? endif; 
+						 <? endif;
 							 endif; ?>
-						 
-						
+
+
 						<? if(!Yii::app()->user->isGuest): ?>
 						<? $user=User::model()->findByPk(Yii::app()->user->dcid);?>
-						
+
 						<div class="btn-group loginbutton">
 								<a class="btn btn-success " href="<? echo Yii::app()->request->baseUrl; ?>/profile"><i class="icon-user icon-white"></i> <? echo Yii::app()->user->name;?> (<span id="pointstotal"><?=$user->points;?></span> Points)</a>
 								<a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#profilemenu"><span class="caret"></span></a>
@@ -289,7 +289,7 @@ if (rand(0, 1)==0):
 					  <?php endif;?>
                </ul>
                   </div>
- 
+
     </div>
   </div>
 </div>
@@ -302,36 +302,36 @@ if (rand(0, 1)==0):
 				<a class="brand" id="mobilebrand" href="http://www.daveconservatoire.org"><img src="<?=bu()."/img/dcvert.png";?>" alt="Dave Conservatoire" style="width:400px" /></a>
 				<div class="navbar">
 					<div class="navbuttons">
-						
+
 						<? if(Yii::app()->urlManager->parseUrl(Yii::app()->request)!="site/subscribe"):?>
 						<a href="<? echo Yii::app()->request->baseUrl;?>/about" class="btn dc-btn-yellow">About</a>
 						<a href="<? echo Yii::app()->request->baseUrl;?>/donate" class="btn dc-btn-orange">Donate</a>
-						<a href="<? echo Yii::app()->request->baseUrl;?>/tuition" class="btn dc-btn-redorange">Personal Tuition</a>			
+						<a href="<? echo Yii::app()->request->baseUrl;?>/tuition" class="btn dc-btn-redorange">Personal Tuition</a>
 						<a href="<? echo Yii::app()->request->baseUrl;?>/contact" class="btn dc-btn-red">Contact</a>
-						
+
 					<? endif;?>
-						
+
 						<?php if (Yii::app()->user->isGuest &&  !isset($_COOKIE['dc_tempusername'])): ?>
-						
+
 						<a href="<? echo Yii::app()->request->baseUrl;?>/login" class="btn dc-btn-red loginbutton">Login</a>
-						
-						<?php 
+
+						<?php
 						endif;
-						if (Yii::app()->user->isGuest &&  isset($_COOKIE['dc_tempusername'])): 
+						if (Yii::app()->user->isGuest &&  isset($_COOKIE['dc_tempusername'])):
 						 $user=User::model()->findByAttributes(array('username'=>$_COOKIE['dc_tempusername']));
 						 if ($user->points>1):
 						 ?>
 						 <a class="btn btn-success loginbutton" href="<? echo Yii::app()->request->baseUrl; ?>/login"><i class="icon-exclamation-sign icon-white"></i> Unclaimed points. Login to save your progress</a>
 						 <? else: ?>
 						 <a href="<? echo Yii::app()->request->baseUrl;?>/login" class="btn dc-btn-red loginbutton">Login</a>
-						 <? endif; 
+						 <? endif;
 							 endif;
 						?>
-						 
-						
+
+
 						<? if(!Yii::app()->user->isGuest): ?>
 						<? $user=User::model()->findByPk(Yii::app()->user->dcid);?>
-						
+
 						<div class="btn-group loginbutton">
 								<a class="btn btn-success " href="<? echo Yii::app()->request->baseUrl; ?>/profile"><i class="icon-user icon-white"></i> <? echo Yii::app()->user->name;?> (<span id="pointstotal"><?=$user->points;?></span> Points)</a>
 								<a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#profilemenu"><span class="caret"></span></a>
@@ -342,10 +342,10 @@ if (rand(0, 1)==0):
 								</ul>
 						</div>
 					  <?php endif;
-						
+
 					  ?>
-					  
-					  
+
+
 					  	<span id="socialmediaicons">
 						<a href="http://www.youtube.com/daveconservatoire" target="_blank"><img class="socialicon" src="<?=bu()."/img/socialicons/youtube.png";?>" /></a>
 						<a href="http://www.twitter.com/dconservatoire" target="_blank"><img class="socialicon" src="<?=bu()."/img/socialicons/twitter.png";?>" /></a>
@@ -362,7 +362,7 @@ if (rand(0, 1)==0):
 <?php echo $content; ?>
 
 
-		
+
 		<div class="navbar" style="margin-bottom: 0px; text-align: center; ">
 		<div class="navbar-inner">
 			<div class="container">
@@ -384,8 +384,8 @@ document.write(d.getFullYear())
 		</div>
 	</div>
 
-		
-		
+
+
 <!-- up to top -->
 <a href="#"><i class="go-top hidden-phone hidden-tablet icon-double-angle-up"></i></a>
 <!--//end-->
